08593794fe3c218be9c435592d52bf9c7543ff14
Rui Sebastiao <ruisebastiao@gmail.com>
2016-08-29 08:29:06 +0100

    updated

--- dist/ng-robotsimulator.js
+++ dist/ng-robotsimulator.js
@@ -7,83 +7,88 @@
  *******************************************************/
 "use strict";
 
+var THREE = THREE;
+
 (function() {
     var robotics = {};
     var Simulator = function() {};
     Simulator.prototype.LoadModel = function() {
         var self = this;
-        if (self.THREE) {
-            var loader = new self.THREE.JSONLoader();
-            loader.load("demo/models/base.json", function(geometry) {
-                var material = new self.THREE.MeshPhongMaterial();
-                var object = new self.THREE.Mesh(geometry, material);
-                self.scene.add(object);
-            });
-        }
+        var loader = new THREE.JSONLoader();
+        loader.load("demo/models/base.json", function(geometry) {
+            var material = new THREE.MeshPhongMaterial();
+            var object = new THREE.Mesh(geometry, material);
+            self.scene.add(object);
+        });
     };
     Simulator.prototype.init = function(element) {
         var self = this;
-        this.THREEService.load().then(function(THREE) {
-            self.THREE = THREE;
-            var container, viewsizeH, viewsizeW, camera, scene, controls;
-            var renderer = self.THREEService.getRenderer();
-            var animation;
-            var init = function() {
-                container = element;
-                viewsizeW = container.clientWidth;
-                viewsizeH = container.clientHeight;
-                renderer.setSize(viewsizeW, viewsizeH);
-                container.appendChild(renderer.domElement);
-                scene = new THREE.Scene();
-                camera = new THREE.PerspectiveCamera(35, viewsizeW / viewsizeH, .1, 1e3);
-                camera.up = new THREE.Vector3(0, 0, 1);
-                camera.lookAt(new THREE.Vector3(0, 0, 0));
-                scene.add(camera);
-                var light = new THREE.DirectionalLight(16777215);
-                light.position.set(1, 1, 1).normalize();
-                scene.add(light);
-                var size = 10;
-                var step = 10;
-                var gridHelper = new THREE.GridHelper(size, step);
-                scene.add(gridHelper);
-                self.THREEPlugins.add("demo/js/TrackballControls.js").then(function() {
-                    controls = new THREE.TrackballControls(camera);
-                    controls.target.set(0, 0, 0);
-                    controls.noZoom = false;
-                    controls.noPan = false;
-                    controls.dynamicDampingFactor = .3;
-                    controls.rotateSpeed = 9;
-                    controls.zoomSpeed = .8;
-                    controls.panSpeed = .8;
-                });
-            };
-            var onWindowResize = function() {
-                var height2 = container.offsetHeight;
-                var width2 = container.offsetWidth;
-                camera.aspect = width2 / height2;
-                camera.updateProjectionMatrix();
-                renderer.setSize(width2, height2);
-                render();
-            };
-            window.addEventListener("resize", onWindowResize, false);
-            var animate = function() {
-                animation = requestAnimationFrame(animate);
-                if (controls) {
-                    controls.update();
-                }
-                render();
-            };
-            var render = function() {
-                renderer.render(scene, camera);
-            };
-            init();
-            self.scene = scene;
-            animate();
-            self.LoadModel();
-        });
+        var container, viewsizeH, viewsizeW, camera, scene, controls;
+        var animation;
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setClearColor(new THREE.Color(12046053));
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.shadowMapEnabled = true;
+        var init = function() {
+            container = element;
+            viewsizeW = container.clientWidth;
+            viewsizeH = container.clientHeight;
+            renderer.setSize(viewsizeW, viewsizeH);
+            container.appendChild(renderer.domElement);
+            scene = new THREE.Scene();
+            camera = new THREE.PerspectiveCamera(35, viewsizeW / viewsizeH, .1, 1e3);
+            camera.position.x = 100;
+            camera.position.y = 100;
+            camera.position.z = 5;
+            camera.lookAt(new THREE.Vector3(0, 0, 0));
+            controls = new THREE.TrackballControls(camera);
+            controls.target.set(0, 0, 0);
+            controls.rotateSpeed = 1;
+            controls.zoomSpeed = .8;
+            controls.panSpeed = .8;
+            scene.add(camera);
+            var light = new THREE.DirectionalLight(16777215);
+            light.position.set(1, 1, 1).normalize();
+            scene.add(light);
+            var size = 10;
+            var step = 10;
+            var gridHelper = new THREE.GridHelper(size, step);
+            scene.add(gridHelper);
+            var geometry = new THREE.TorusKnotGeometry(100, 30, 100, 16);
+            var material = new THREE.MeshDepthMaterial({
+                color: 6710886,
+                wireframe: true,
+                wireframeLinewidth: 1
+            });
+            var torus = new THREE.Mesh(geometry, material);
+            torus.name = "Torus";
+            scene.add(torus);
+        };
+        var onWindowResize = function() {
+            var height2 = container.offsetHeight;
+            var width2 = container.offsetWidth;
+            camera.aspect = width2 / height2;
+            camera.updateProjectionMatrix();
+            renderer.setSize(width2, height2);
+            render();
+        };
+        window.addEventListener("resize", onWindowResize, false);
+        var animate = function() {
+            animation = requestAnimationFrame(animate);
+            if (controls) {
+                controls.update();
+            }
+            render();
+        };
+        var render = function() {
+            renderer.render(scene, camera);
+        };
+        init();
+        self.scene = scene;
+        animate();
     };
     robotics.Simulator = Simulator;
-    robotics.SimulatorDirective = function($timeout, THREEService, THREEPlugins) {
+    robotics.SimulatorDirective = function($timeout) {
         return {
             restrict: "E",
             scope: {
@@ -93,8 +98,6 @@
             template: '<div class="scenecontainer"></div>',
             link: function(scope, element) {
                 var Simulator = new robotics.Simulator();
-                Simulator.THREEService = THREEService;
-                Simulator.THREEPlugins = THREEPlugins;
                 element.ready(function() {
                     $timeout(function() {
                         Simulator.init(element[0].children[0]);
@@ -103,5 +106,5 @@
             }
         };
     };
-    angular.module("robotics.simulator", [ "threejs" ]).directive("roboticsSimulator", robotics.SimulatorDirective);
+    angular.module("robotics.simulator", []).directive("roboticsSimulator", robotics.SimulatorDirective);
 })();
--- src/ng-robotsimulator.js
+++ src/ng-robotsimulator.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var THREE = THREE;
 (function () {
 
   var robotics = {};
@@ -14,162 +14,178 @@
 
   Simulator.prototype.LoadModel = function () {
     var self = this;
-    if (self.THREE) {
 
+    var loader = new THREE.JSONLoader();
+    // load a resource
+    loader.load(
+      // resource URL
+      'demo/models/base.json',
+      // Function when resource is loaded
+      function (geometry) {
+        var material = new THREE.MeshPhongMaterial();
+        var object = new THREE.Mesh(geometry, material);
 
-      var loader = new self.THREE.JSONLoader();
-      // load a resource
-      loader.load(
-        // resource URL
-        'demo/models/base.json',
-        // Function when resource is loaded
-        function (geometry) {
-          var material = new self.THREE.MeshPhongMaterial();
-          var object = new self.THREE.Mesh(geometry, material);
+        self.scene.add(object);
+      }
 
-          self.scene.add(object);
-        }
+    );
 
-      );
-    }
   };
 
   Simulator.prototype.init = function (element) {
     var self = this;
-    this.THREEService.load().then(function (THREE) {
-      self.THREE = THREE;
-
-      var container, viewsizeH, viewsizeW, camera, scene, controls;
-
-      var renderer = self.THREEService.getRenderer();
-      var animation;
 
 
-
-      var init = function () {
-        container = element;
-        viewsizeW = container.clientWidth;
-        viewsizeH = container.clientHeight;
-
-        renderer.setSize(viewsizeW, viewsizeH);
-        container.appendChild(renderer.domElement);
+    var container, viewsizeH, viewsizeW, camera, scene, controls;
 
 
-        scene = new THREE.Scene();
+    var animation;
 
-        camera = new THREE.PerspectiveCamera(35, viewsizeW / viewsizeH, 0.1, 1000);
-        // camera = new THREE.PerspectiveCamera(50, 1, 150, 650);
-       	// camera.position.set(0, 0, 5);
-        camera.up = new THREE.Vector3(0, 0, 1);
-        camera.lookAt(new THREE.Vector3(0, 0, 0));
-        scene.add(camera);
-
-        // camera.position.z = 500;
-        //890
-
-        var light = new THREE.DirectionalLight(0xffffff);
-        light.position.set(1, 1, 1).normalize();
-        scene.add(light);
-
-        var size = 10;
-        var step = 10;
-
-        var gridHelper = new THREE.GridHelper(size, step);
-        scene.add(gridHelper);
-
-        self.THREEPlugins.add('demo/js/TrackballControls.js').then(function () {
-
-          controls = new THREE.TrackballControls(camera);
-          controls.target.set(0, 0, 0);
-          controls.noZoom = false;
-          controls.noPan = false;
-          controls.dynamicDampingFactor = 0.3;
-          controls.rotateSpeed = 9.0;
-          controls.zoomSpeed = 0.8;
-          controls.panSpeed = 0.8;
-
-        
-
-        });
-
-        // var editcontrol=new THREE.EditorControls( camera, camera, renderer.domElement );
-        // scene.add(editcontrol);
-
-        // var ambientLight = new THREE.AmbientLight(0xfffff);
-        // scene.add(ambientLight);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(new THREE.Color(0xb7cee5));
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMapEnabled = true;
 
 
-        // var ambientLight = new THREE.AmbientLight(0x55555);
-        // scene.add(ambientLight);
+    var init = function () {
+      container = element;
+      viewsizeW = container.clientWidth;
+      viewsizeH = container.clientHeight;
 
-        // var geometry = new THREE.TorusKnotGeometry(100, 30, 100, 16);
-
-        // var material = new THREE.MeshDepthMaterial({
-        //   color: 0x666666,
-        //   wireframe: true,
-        //   wireframeLinewidth: 1
-        // });
-
-        // var torus = new THREE.Mesh(geometry, material);
-        // torus.name = "Torus";
-        // scene.add(torus);
+      renderer.setSize(viewsizeW, viewsizeH);
+      container.appendChild(renderer.domElement);
 
 
-      };
+      scene = new THREE.Scene();
+
+      camera = new THREE.PerspectiveCamera(35, viewsizeW / viewsizeH, 0.1, 1000);
+      // camera = new THREE.PerspectiveCamera(50, 1, 150, 650);
+      // camera.position.set(0, 0, 5);
+      // camera.up = new THREE.Vector3(0, 0, 1);
+      // camera.lookAt(new THREE.Vector3(0, 0, 0));
+      // scene.add(camera);
+
+      camera.position.x = 100;
+      camera.position.y = 100;
+      camera.position.z = 5;
+      camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+
+      controls = new THREE.TrackballControls(camera);
+						controls.target.set(0, 0, 0);
+
+						controls.rotateSpeed = 1.0;
+						controls.zoomSpeed = 0.8;
+						controls.panSpeed = 0.8;
+
+						scene.add(camera);
+
+      // camera.position.z = 500;
+      //890
+
+      var light = new THREE.DirectionalLight(0xffffff);
+      light.position.set(1, 1, 1).normalize();
+      scene.add(light);
+
+      var size = 10;
+      var step = 10;
+
+      var gridHelper = new THREE.GridHelper(size, step);
+      scene.add(gridHelper);
+
+      // self.THREEPlugins.add('demo/js/TrackballControls.js').then(function () {
+
+      //   controls = new THREE.TrackballControls(camera);
+      //   // controls.target.set(0, 0, 0);
+      //   controls.noZoom = false;
+      //   controls.noPan = false;
+      //   // controls.dynamicDampingFactor = 0.3;
+      //   // controls.rotateSpeed = 9.0;
+      //   // controls.zoomSpeed = 0.8;
+      //   // controls.panSpeed = 0.8;
 
 
 
-      // -----------------------------------
-      // Event listeners
-      // -----------------------------------
-      // Cancel animation when view route or state changes
-      // eg. watch for state change when using ui-router:
-      // $rootScope.$on('$stateChangeStart', function () {
-      //   cancelAnimationFrame(animation);
       // });
 
-      //
-      var onWindowResize = function () {
-        //camera.aspect = width-10 / height-10;
-        // var container2 = elem[0].parentElement;
+      // var editcontrol=new THREE.EditorControls( camera, camera, renderer.domElement );
+      // scene.add(editcontrol);
+
+      // var ambientLight = new THREE.AmbientLight(0xfffff);
+      // scene.add(ambientLight);
 
 
-        var height2 = container.offsetHeight;
-        var width2 = container.offsetWidth;
-        camera.aspect = width2 / height2;
-        camera.updateProjectionMatrix();
-        renderer.setSize(width2, height2);
+      // var ambientLight = new THREE.AmbientLight(0x55555);
+      // scene.add(ambientLight);
 
-        render();
+      var geometry = new THREE.TorusKnotGeometry(100, 30, 100, 16);
 
-      };
+      var material = new THREE.MeshDepthMaterial({
+        color: 0x666666,
+        wireframe: true,
+        wireframeLinewidth: 1
+      });
 
-      window.addEventListener('resize', onWindowResize, false);
+      var torus = new THREE.Mesh(geometry, material);
+      torus.name = "Torus";
+      scene.add(torus);
 
 
-      // -----------------------------------
-      // Draw and Animate
-      // -----------------------------------
-      var animate = function () {
-        animation = requestAnimationFrame(animate);
-        if (controls) {
-          controls.update();
+    };
 
-        }
-        render();
-      };
 
-      var render = function () {
 
-        renderer.render(scene, camera); // forceClear == true
-      };
+    // -----------------------------------
+    // Event listeners
+    // -----------------------------------
+    // Cancel animation when view route or state changes
+    // eg. watch for state change when using ui-router:
+    // $rootScope.$on('$stateChangeStart', function () {
+    //   cancelAnimationFrame(animation);
+    // });
 
-      init();
-      self.scene = scene;
-      animate();
-      self.LoadModel();
+    //
+    var onWindowResize = function () {
+      //camera.aspect = width-10 / height-10;
+      // var container2 = elem[0].parentElement;
 
-    });
+
+      var height2 = container.offsetHeight;
+      var width2 = container.offsetWidth;
+      camera.aspect = width2 / height2;
+      camera.updateProjectionMatrix();
+      renderer.setSize(width2, height2);
+
+      render();
+
+    };
+
+    window.addEventListener('resize', onWindowResize, false);
+
+
+    // -----------------------------------
+    // Draw and Animate
+    // -----------------------------------
+    var animate = function () {
+      animation = requestAnimationFrame(animate);
+      if (controls) {
+        controls.update();
+
+      }
+      render();
+    };
+
+    var render = function () {
+
+      renderer.render(scene, camera); // forceClear == true
+    };
+
+    init();
+    self.scene = scene;
+    animate();
+//    self.LoadModel();
+
+
 
 
   };
@@ -178,7 +194,7 @@
   /**
    *   Angular Robotics Simulator directive
    */
-  robotics.SimulatorDirective = function ($timeout, THREEService, THREEPlugins) {
+  robotics.SimulatorDirective = function ($timeout) {
     return {
       restrict: 'E',
       scope: {
@@ -189,8 +205,7 @@
       link: function (scope, element) {
 
         var Simulator = new robotics.Simulator();
-        Simulator.THREEService = THREEService;
-        Simulator.THREEPlugins = THREEPlugins;
+
         element.ready(function () {
           $timeout(function () {
             Simulator.init(element[0].children[0]);
@@ -198,10 +213,9 @@
         });
 
 
-
       }
     };
   };
 
-  angular.module('robotics.simulator', ['threejs']).directive('roboticsSimulator', robotics.SimulatorDirective);
+  angular.module('robotics.simulator', []).directive('roboticsSimulator', robotics.SimulatorDirective);
 })();
